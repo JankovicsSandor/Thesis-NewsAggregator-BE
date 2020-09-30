8512055c89e50c50fa4fc7cff8c492bdf411f5c5
JankovicsSandor <sanyika717022@gmail.com>
2020-09-30 10:54:15 +0200

    Add user api scheme

---
+++ Services/User/User.API/DependencyInjection/BussinessLogicInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User.BussinessLogic.UnitOfWork.Password;
+
+namespace User.API.DependencyInjection
+{
+    public static class BussinessLogicInjection
+    {
+        public static void RegisterBussinessLogicInjection(this IServiceCollection services)
+        {
+            services.AddTransient<IPasswordUnitOfWork, PasswordUnitOfWork>();
+            services.AddTransient<IPasswordUnitOfWork, PasswordUnitOfWork>();
+        }
+    }
+}
---
+++ Services/User/User.BussinessLogic/UnitOfWork/User/UserUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using User.BussinessLogic.UnitOfWork.Password;
+
+namespace User.BussinessLogic.UnitOfWork.User
+{
+    public class UserUnitOfWork
+    {
+        private IPasswordUnitOfWork _passwordLogic;
+
+        public UserUnitOfWork(IPasswordUnitOfWork passwordLogic)
+        {
+            _passwordLogic = passwordLogic;
+        }
+
+        public void CreateNewUser()
+    }
+}
