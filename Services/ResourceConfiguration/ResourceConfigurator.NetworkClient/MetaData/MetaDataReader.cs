d59e186cfa7f83d92152578226f5af096eba17b6
JankovicsSandor <sanyika717022@gmail.com>
2020-11-01 19:17:16 +0100

    Add metadata reader from HTML reader

---
+++ Services/ResourceConfiguration/ResourceConfigurator.NetworkClient/MetaData/MetaDataReader.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using ResourceConfigurator.Shared.Models.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceConfigurator.NetworkClient.MetaData
+{
+    public class MetaDataReader : IMetaDataReader
+    {
+        public WebsiteMetaData GetWebsiteMetadata(string url)
+        {
+            // Get the URL specified
+            var webGet = new HtmlWeb();
+            var document = webGet.Load(url);
+            var metaTags = document.DocumentNode.SelectNodes("//meta");
+            WebsiteMetaData metaInfo = new WebsiteMetaData();
+            if (metaTags != null)
+            {
+                foreach (var tag in metaTags)
+                {
+                    var tagContent = tag.Attributes["content"];
+                    var tagProperty = tag.Attributes["property"];
+                    if (tagProperty != null && tagContent != null)
+                    {
+                        switch (tagProperty.Value.ToLower())
+                        {
+                            case "og:title":
+                                metaInfo.Title = string.IsNullOrEmpty(metaInfo.Title) ? tagContent.Value : metaInfo.Title;
+                                break;
+                            case "og:description":
+                                metaInfo.Description = string.IsNullOrEmpty(metaInfo.Description) ? tagContent.Value : metaInfo.Description;
+                                break;
+                            case "og:image":
+                                metaInfo.ImageUrl = string.IsNullOrEmpty(metaInfo.ImageUrl) ? tagContent.Value : metaInfo.ImageUrl;
+                                break;
+                        }
+                    }
+                }
+            }
+            return metaInfo;
+        }
+    }
+}
