7f4e31aa8be58c751d2b4f475424f30e4098f44f
JankovicsSandor <sanyika717022@gmail.com>
2020-11-01 19:21:02 +0100

    Retrive deleted source

---
+++ Services/ResourceConfiguration/ResourceConfigurator.NetworkClient/ResourceToDataNetworkClient.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using ResourceConfigurator.Shared.Event;
+using ResourceConfigurator.Shared.Models.Events;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceConfigurator.NetworkClient
+{
+    public class ResourceToDataNetworkClient : IResourceToDataNetworkClient
+    {
+        private HttpClient _client;
+
+        public ResourceToDataNetworkClient(HttpClient client)
+        {
+            _client = client;
+            string url = Environment.GetEnvironmentVariable("APPSETTING_DATA_URL");
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new Exception("BaseUrl is empty");
+            }
+            _client.BaseAddress = new Uri(url);
+        }
+
+        public async Task AddNewArticleToData(AddNewArticleEvent newItem)
+        {
+            HttpResponseMessage response = await _client.PostAsync("api/article", new StringContent(JsonConvert.SerializeObject(newItem), Encoding.UTF8, "application/json"));
+
+            string content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(content);
+            }
+        }
+
+        public async Task<int> AddNewResourceToData(AddNewResourceEvent newItem)
+        {
+            HttpResponseMessage response = await _client.PostAsync("api/resource", new StringContent(JsonConvert.SerializeObject(newItem), Encoding.UTF8, "application/json"));
+
+            string content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(content);
+            }
+
+            AddNewResourceEventResponse responseContent = JsonConvert.DeserializeObject<AddNewResourceEventResponse>(content);
+
+            return responseContent.Id;
+
+
+        }
+
+
+    }
+}
--- Services/ResourceConfiguration/ResourceConfigurator.NetworkClient/SyndicationFeedReader/FeedReader.cs
+++ Services/ResourceConfiguration/ResourceConfigurator.NetworkClient/SyndicationFeedReader/FeedReader.cs
@@ -1,6 +1,8 @@
 ﻿using ResourceConfiguration.API.Models.Output;
+using ResourceConfigurator.NetworkClient.MetaData;
 using ResourceConfigurator.Shared.Event;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
@@ -12,6 +14,33 @@ namespace ResourceConfigurator.NetworkClient.SyndicationFeedReader
 {
     public class FeedReader : IFeedReader
     {
+        private IMetaDataReader _metaDataReader;
+
+        public FeedReader(IMetaDataReader metaReader)
+        {
+            _metaDataReader = metaReader;
+        }
+
+        public ResourcePropertiesModel GetResourceProperties(string url)
+        {
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            SyndicationItem item = feed.Items.FirstOrDefault();
+            return new ResourcePropertiesModel()
+            {
+                Description = item.Summary.Text,
+                Title = item.Title.Text,
+                Link = item.Links.FirstOrDefault().Uri.ToString(),
+                PublishDate = item.PublishDate.DateTime,
+                Author = new ResourceProvider()
+                {
+                    Picture = feed.ImageUrl.ToString()
+                }
+
+            };
+        }
 
         public string GetResourceProfilePicture(string url)
         {
@@ -29,15 +58,21 @@ namespace ResourceConfigurator.NetworkClient.SyndicationFeedReader
             SyndicationFeed feed = SyndicationFeed.Load(reader);
             reader.Close();
 
-            IEnumerable<AddNewArticleEvent> articles = feed.Items.Select(item => new AddNewArticleEvent()
-            {
-                Description = item.Summary.Text,
-                Title = item.Title.Text,
-                Link = item.Links.FirstOrDefault().Uri.ToString(),
-                PublishDate = item.PublishDate.DateTime,
-                // TODO Add picture
-            });
+            ConcurrentBag<AddNewArticleEvent> articles = new ConcurrentBag<AddNewArticleEvent>();
 
+            Parallel.ForEach(feed.Items, (currentArticle) =>
+            {
+                string newsUrl = currentArticle.Links.FirstOrDefault().Uri.ToString();
+                AddNewArticleEvent result = new AddNewArticleEvent()
+                {
+                    Description = currentArticle.Summary.Text,
+                    Title = currentArticle.Title.Text,
+                    Link = currentArticle.Links.FirstOrDefault().Uri.ToString(),
+                    PublishDate = currentArticle.PublishDate.DateTime,
+                    // TODO Add picture
+                    Picture = _metaDataReader.GetWebsiteMetadata(newsUrl).ImageUrl
+                };
+            });
             return articles;
         }
     }
