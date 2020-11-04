29de5c38d54f778ce1cbfafc182735c4d9a327e0
JankovicsSandor <sanyika717022@gmail.com>
2020-11-04 19:44:47 +0100

    Move location of database

---
+++ Services/Reader/Reader.DataAccess/Database/ArticleGroup.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Reader.DataAccess.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News.DataAccess.Database
+{
+    public class ArticleGroup
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Similar")]
+        public Article Similar { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
