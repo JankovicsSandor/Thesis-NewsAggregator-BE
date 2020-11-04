0448cdb37dd54a4b739caa73f361c3a244d9e471
JankovicsSandor <sanyika717022@gmail.com>
2020-11-04 19:52:59 +0100

    Add new article  event handler

---
+++ Services/News/News.Shared/Events/AddNewArticleEvent.cs
@@ -0,0 +1,17 @@
+﻿using EventBusRabbitMQ.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News.Shared.Events
+{
+    public class AddNewArticleEvent : IntegrationEvent
+    {
+        public int FeedId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Picture { get; set; }
+    }
+}
