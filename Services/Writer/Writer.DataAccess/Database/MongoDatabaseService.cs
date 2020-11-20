7dcf746d0fcbeada505b61b756bfc5b8b78325ba
JankovicsSandor <sanyika717022@gmail.com>
2020-11-20 19:22:41 +0100

    Add mongoDatabase service

---
+++ Services/Writer/Writer.DataAccess/Database/MongoDatabaseService.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Writer.DataAccess.Database
+{
+    public class MongoDatabaseService
+    {
+        private IMongoDatabase _context;
+
+        public MongoDatabaseService()
+        {
+            string mongoConnectionString = Environment.GetEnvironmentVariable("MONGODB_DATABASE");
+            if (string.IsNullOrEmpty(mongoConnectionString))
+            {
+                throw new Exception("MongoConnectionstring is empty");
+            }
+
+            string mongoDatabase = Environment.GetEnvironmentVariable("MONGODB_COLLECTION_NAME");
+            if (string.IsNullOrEmpty(mongoDatabase))
+            {
+                throw new Exception("MongoDatabase name is empty");
+            }
+            var client = new MongoClient(mongoConnectionString);
+            _context = client.GetDatabase(mongoDatabase);
+        }
+    }
+}
