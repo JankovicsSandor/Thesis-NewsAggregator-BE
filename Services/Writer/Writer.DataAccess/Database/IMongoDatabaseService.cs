33c24d5f66d4af3902facace94aca3ba24bd2af9
JankovicsSandor <sanyika717022@gmail.com>
2020-11-21 18:37:08 +0100

    Add ignored mongoDatabase service interface

---
+++ Services/Writer/Writer.DataAccess/Database/IMongoDatabaseService.cs
@@ -0,0 +1,11 @@
+﻿using News.DataAccess.Database;
+using System.Collections.Generic;
+
+namespace Writer.DataAccess.Database
+{
+    public interface IMongoDatabaseService
+    {
+        void AddArticleGroup(ArticleGroup newGroup);
+        List<ArticleGroup> GetAllArticleGroup();
+    }
+}
