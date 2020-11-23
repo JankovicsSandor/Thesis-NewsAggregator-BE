17b41b61c995a2038d20b81cee1c1e8ac173bc6d
JankovicsSandor <sanyika717022@gmail.com>
2020-11-23 18:45:50 +0100

    Add new group done handler test

---
+++ Services/Writer/Writer.UnitTests/NewsGroupDoneEventHandlerTest.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Writer.BussinessLogic.EventHandler;
+using Writer.BussinessLogic.ExternalDataProvider.NewsData;
+using Writer.DataAccess.Database;
+using Writer.Shared.Events;
+
+namespace Writer.UnitTests
+{
+    public class NewsGroupDoneEventHandlerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Handle_Test_When_NullParam_Is_Given()
+        {
+            Assert.Throws<ArgumentNullException>(() => new NewsGroupDoneEventHandler(null, null));
+        }
+
+        [Test]
+        public async Task Handle_Test_When_Correct_Param_Is_Given_Fresh_Database()
+        {
+            Mock<INewsHttpClient> newsHttpClient = new Mock<INewsHttpClient>();
+            Mock<IMongoDatabaseService> mongoService = new Mock<IMongoDatabaseService>();
+            NewsGroupDoneEventHandler handler = new NewsGroupDoneEventHandler(newsHttpClient.Object, mongoService.Object);
+
+            await handler.Handle(new NewsGroupDoneEvent()
+            {
+                NewsItem = new NewsGroupDoneNewsItem()
+            });
+            newsHttpClient.Verify(e => e.GetArticleFromDescription(It.IsAny<string>()), Times.Never);
+            mongoService.Verify(e => e.AddArticleGroup(It.IsAny<ArticleGroup>()), Times.Once);
+        }
+    }
+}
